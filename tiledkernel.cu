4fc6f73cd442e775b41101ed75d4764f0998eb6c
Stanley Christopher West II <schristopherwest@gmail.com>
2026-08-14 02:33:53 -0500

    sgemm: double-buffered shared tiles, 21/21, 43.3 TFLOP/s (81% of cuBLAS)

--- tiledkernel.cu
+++ tiledkernel.cu
@@ -50,8 +50,8 @@ void tiledMult(float* m1, float* m2, float* m3, unsigned int R, unsigned int C, 
     // computation.
 
     // Mds is 8 wide, 32 tall. Nds is 128 wide, 2 tall.
-    __shared__ float Mds[BK][TILE_WIDTH]; // a shared memory array 8 * 128 = 1024 large
-    __shared__ float Nds[BK][TILE_WIDTH]; // together, we have 2048 elements
+    __shared__ float Mds[2][BK][TILE_WIDTH]; // a shared memory array 8 * 128 = 1024 large
+    __shared__ float Nds[2][BK][TILE_WIDTH]; // together, we have 2048 elements
 
     // In CUDA, x is horizontal and y is vertical. In a matrix, you write m[row][col],
     // which is vertical first. That is why row is built from by/ty and column is
@@ -78,9 +78,33 @@ void tiledMult(float* m1, float* m2, float* m3, unsigned int R, unsigned int C, 
 
     float acc[TM][TN] = {0}; // stores all 64 outputs of each thread
 
+    // These two copies of the fill loop are hardcoded to batch 0 and write into
+    // buffer 0. This is called a prologue and primes the first buffer before
+    // we start.
+
+    for (int s = 0; s < TILE_WIDTH; s+= (BLOCK_DIM*BLOCK_DIM)/BK)
+    {
+        int r = by*TILE_WIDTH + aRow + s;
+        int k = aValue;
+        Mds[0][aValue][aRow + s] = (r < R && k < K) ? m1[r*K + k] : 0.0f;
+    }
+
+    #pragma unroll
+    for (int s = 0; s < BK; s += (BLOCK_DIM*BLOCK_DIM)/TILE_WIDTH)
+    {
+        int k = bValue + s;
+        int c = bx*TILE_WIDTH + bCol;
+        Nds[0][bValue + s][bCol] = (k < K && c < C) ? m2[k*C + c] : 0.0f;
+    }
+
+    __syncthreads();
+
     // "i" represents which BATCH. this loops over (2048 + 8 - 1)/8 = 256 times.
     for (int i = 0; i < (K + BK - 1)/BK; ++i)
     {
+        int cur = i % 2; // computes which buffer holds the batch being computed
+        int nxt = 1 - cur; // computes which buffer being filled
+
         // #pragma unroll tells the compiler to replace the loop with copies of its
         // body, one per iteration. this allows all variables to be placed in
         // registers rather than local memory
@@ -89,45 +113,47 @@ void tiledMult(float* m1, float* m2, float* m3, unsigned int R, unsigned int C, 
         // is just the first index in the matrix. For m1 it is K, and for m2 it is C.
         // Our m1 has a K of 8, and our m2 has a C of 128.
 
-        #pragma unroll
-        // This loop fills the Mds array. We start by computing the first 0-7 k terms.
-        // Remember, m1 is [row][k]. Row is 128, so we iterate through 128, jumping
-        // by 32 each time. For s=0, values 0-31 get filled, for s=1, values 32-63
-        // get filled, and so on. This is because we copy the chunk's width,
-        // take a quarter of its height, and stamp it 4 times. This tells us the chunk's
-        // step and cap. For m1 (width = 8 and height = 128), we say 128/4 is 32, so step
-        // = 32, and the cap is 32x4, or 128.
-        for (int s = 0; s < TILE_WIDTH; s+= (BLOCK_DIM*BLOCK_DIM)/BK)
+        // end of loop guard to protect against i=255
+        if (i + 1 < (K + BK - 1)/BK)
         {
-            // by*TILE_WIDTH gives you which group of 128 you start at. aRow + s tells
-            // you how far down to go in that block. It gives an absolute row of m1, from
-            // 0 to 2047
-            int r = by*TILE_WIDTH + aRow + s;
-            // gives you an absolute COLUMN. i*BK computes the trip, while aValue tells
-            // you how far right to go
-            int k = i*BK + aValue;
-            // the right side, m1[r*K + k], fetches row r, column k of m1. the left side,
-            // Mds[aValue][aRow + s] is an index into the Mds array but reads as [k][row].
-            Mds[aValue][aRow + s] = (r < R && k < K) ? m1[r*K + k] : 0.0f;
-        }
+            #pragma unroll
+            // This loop fills the Mds array. We start by computing the first 0-7 k terms.
+            // Remember, m1 is [row][k]. Row is 128, so we iterate through 128, jumping
+            // by 32 each time. For s=0, values 0-31 get filled, for s=1, values 32-63
+            // get filled, and so on. This is because we copy the chunk's width,
+            // take a quarter of its height, and stamp it 4 times. This tells us the chunk's
+            // step and cap. For m1 (width = 8 and height = 128), we say 128/4 is 32, so step
+            // = 32, and the cap is 32x4, or 128.
+            for (int s = 0; s < TILE_WIDTH; s+= (BLOCK_DIM*BLOCK_DIM)/BK)
+            {
+                // by*TILE_WIDTH gives you which group of 128 you start at. aRow + s tells
+                // you how far down to go in that block. It gives an absolute row of m1, from
+                // 0 to 2047
+                int r = by*TILE_WIDTH + aRow + s;
+                // gives you an absolute COLUMN. i*BK computes the trip, while aValue tells
+                // you how far right to go
+                int k = (i+1)*BK + aValue;
+                // the right side, m1[r*K + k], fetches row r, column k of m1. the left side,
+                // Mds[aValue][aRow + s] is an index into the Mds array but reads as [k][row].
+                Mds[nxt][aValue][aRow + s] = (r < R && k < K) ? m1[r*K + k] : 0.0f;
+            }
 
-        // This loop filles the Nds array. We also start by computing the first 0-7
-        // k terms. m2 is [k][row], so we index through BK first, jumping by 2 each
-        // time. For s=0, values 0-1 get filled, for s=1, values 2-3 get filled,
-        // and so on. Like previously, m2 (width = 128 and height = 8), we say 8/2 is
-        // 2, so step = 2, and 2x4 is 8, so cap = 8.
-        #pragma unroll
-        for (int s = 0; s < BK; s += (BLOCK_DIM*BLOCK_DIM)/TILE_WIDTH)
-        {
-            // inverse of Mds. k grabs the absolute row
-            int k = i*BK + bValue + s;
-            // grabs the absolute column
-            int c = bx*TILE_WIDTH + bCol;
-            // writes row, col. does not swap like Mds that writes col, row
-            Nds[bValue + s][bCol] = (k < K && c < C) ? m2[k*C + c] : 0.0f;
+            // This loop filles the Nds array. We also start by computing the first 0-7
+            // k terms. m2 is [k][row], so we index through BK first, jumping by 2 each
+            // time. For s=0, values 0-1 get filled, for s=1, values 2-3 get filled,
+            // and so on. Like previously, m2 (width = 128 and height = 8), we say 8/2 is
+            // 2, so step = 2, and 2x4 is 8, so cap = 8.
+            #pragma unroll
+            for (int s = 0; s < BK; s += (BLOCK_DIM*BLOCK_DIM)/TILE_WIDTH)
+            {
+                // inverse of Mds. k grabs the absolute row
+                int k = (i+1)*BK + bValue + s;
+                // grabs the absolute column
+                int c = bx*TILE_WIDTH + bCol;
+                // writes row, col. does not swap like Mds that writes col, row
+                Nds[nxt][bValue + s][bCol] = (k < K && c < C) ? m2[k*C + c] : 0.0f;
+            }
         }
-
-        __syncthreads(); // makes sure each thread is finished loading its values
 
         // j runs 0-7 for 256 batches, loading 8 * 256 = 2048 values total
         #pragma unroll
@@ -141,16 +167,16 @@ void tiledMult(float* m1, float* m2, float* m3, unsigned int R, unsigned int C, 
                 // Mds is stored at 8 rows of 128. j walks each row. ty is the
                 // specific thread id. CONTIG multiplies by 1 on each iteration,
                 // picking 8 elements out of the 128 elements on each row.
-                a[y] = Mds[j][VEC*ty + y*CONTIG];
-                a[y+4] = Mds[j][VEC*ty + HALF + y*CONTIG];
+                a[y] = Mds[cur][j][VEC*ty + y*CONTIG];
+                a[y+4] = Mds[cur][j][VEC*ty + HALF + y*CONTIG];
             }
 
             #pragma unroll
             for (int y = 0; y < VEC; ++y)
             {
                 // same function but it walks the column
-                b[y] = Nds[j][VEC*tx + y*CONTIG];
-                b[y+4] = Nds[j][VEC*tx + HALF + y*CONTIG];
+                b[y] = Nds[cur][j][VEC*tx + y*CONTIG];
+                b[y+4] = Nds[cur][j][VEC*tx + HALF + y*CONTIG];
             }
 
             #pragma unroll
